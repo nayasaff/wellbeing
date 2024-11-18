23d15fc073ef63a45ed6494e3be0813551b4f17d
nayasaff <94166588+nayasaff@users.noreply.github.com>
2024-11-18 13:20:21 +0200

    Final commit

--- wellbeing/settings.py
+++ wellbeing/settings.py
@@ -185,6 +185,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = [os.path.join(BASE_DIR, 'accounts/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
